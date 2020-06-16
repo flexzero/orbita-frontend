bfa4e1408086aa17276cf5c8758aa04ee6c2cd06
flexzero <flexzero2016@gmail.com>
2020-06-16 13:40:42 +0530

    fixed port

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,7 +1,6 @@
 export default function baseUrl(endpoint, secure = false) {
-  const local = "http://localhost:1337/api/";
-  const remote = "http://52.74.250.49:3003/api/"
+  const remote = "http://52.74.250.49:1337/api/"
 
-  if (secure) return `${local}secure/${endpoint}`;
-  return `${local}${endpoint}`;
+  if (secure) return `${remote}secure/${endpoint}`;
+  return `${remote}${endpoint}`;
 }
